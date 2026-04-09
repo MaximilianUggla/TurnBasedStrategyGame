44b9702958429071c59f45b38879f6e8e96a9340
Maximilan Waldenfeldt Uggla <maxi.uggla@icloud.com>
2026-04-09 15:25:20 +0200

    first teration of the secound hexgrid (nxLayers)

---
+++ Unity/Galaxy Interregnum/Assets/Script/HexRingMetrics.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public static class HexRingMetrics
+{
+    public static float SideRadius (float hexSize)
+    {
+        return hexSize * 0.866025404f;
+    }
+
+    public static Vector3[] Corners (float hexSize)
+    {
+        Vector3[] corners = new Vector3[6];
+        for (int i = 0; i < 6; i++)
+        {
+            corners[i] = Corner(hexSize, i);
+        }
+        return corners;
+    }
+
+    public static Vector3 Corner (float hexSize, int index)
+    {
+        float angle = 60f * index;
+
+        Vector3 corner = new Vector3(hexSize * Mathf.Cos(angle * Mathf.Deg2Rad), 
+        0f,
+        hexSize * Mathf.Sin(angle * Mathf.Deg2Rad)
+        );
+
+        return corner;
+    }
+
+    public static Vector3 Center(float cornerRadius, int col, int r, int padding)
+    {
+        float factor;
+
+        if (col % 2 != 0)
+        {
+            if (r == 1 || r == -1)
+            {
+                if (r < 0) {factor = 1;}
+                else {factor = -1;}
+            }
+            else {
+                if (r < 0) {factor = (2 * r + 1);}
+                else {factor = 2 * r - 1;}
+            }
+        }
+        else {factor = 2 * r;}
+
+        Vector3 centerPosition;
+        centerPosition.x = col * cornerRadius * 1.5f + col * padding;
+        centerPosition.y = 0f;
+        centerPosition.z = factor * (SideRadius(cornerRadius)) + r * padding;
+
+        return centerPosition;
+    }
+}
